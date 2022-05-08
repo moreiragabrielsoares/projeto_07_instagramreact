adc30bc9cfe489aadad477ddbfad87a4fbdada4b
Gabriel Moreira <moreira.gabrielsoares@gmail.com>
2022-05-08 12:31:34 -0300

    Macro componentização implementada

---
+++ src/Content.js
@@ -0,0 +1,13 @@
+
+import Sidebar from './Sidebar';
+import PrincipalContent from './PrincipalContent';
+
+
+export default function Content() {
+    return (
+        <div class="corpo">
+            <PrincipalContent />
+            <Sidebar />
+        </div>
+    )
+}
---
+++ src/PrincipalContent.js
@@ -0,0 +1,15 @@
+
+import Stories from './Stories';
+import Posts from './Posts';
+
+
+export default function PrincipalContent() {
+    return (
+        <div class="esquerda">
+            
+            <Stories />
+            <Posts />
+            
+        </div>
+    )
+}
